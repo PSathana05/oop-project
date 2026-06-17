185de5cff7f32547d432757398dc258a89d69ccb
prabalinipraba60-svg <prabalinipraba60-svg@gmail.com>
2026-06-17 13:19:59 +0530

    //remove System.out.println

--- So.Eng/src/FlightFileWriter.java
+++ So.Eng/src/FlightFileWriter.java
@@ -1,10 +1,13 @@
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class FlightFileWriter() {
+public class FlightFileWriter{
+    private FlightFileWriter(){
+        throw new IllegalStateException("Utility class");
+    }
     public static boolean writeFile(String flightsFile) {
-        try {
-            FileWriter writer = new FileWriter(flightsFile); // creates or overwrites file
+        try (
+            FileWriter writer = new FileWriter(flightsFile)){ // creates or overwrites file
             writer.write("UL101,Colombo,London,5\n");
             writer.write("EK225,Dubai,New York,10\n");
             writer.write("QR320,Doha,Paris,8\n");
@@ -15,10 +18,10 @@ public class FlightFileWriter() {
             writer.write("QF402,Sydney,Melbourne,15\n");
             writer.write("MH116,Kuala Lumpur,Perth,10\n");
             writer.write("TK730,Istanbul,Colombo,11\n");
-            writer.close();
+
             return true;
         } catch (IOException e) {
-            System.out.println("Error writing to file: " + e.getMessage());
+
             return false;
         }
     }
